fc68ce7a6af257f60812af42657b96bcc71ceb52
Sami Adel <sami.adel.galal02@gmail.com>
2024-01-23 15:27:09 +0200

    add icons

--- app/_components/hero-section.tsx
+++ app/_components/hero-section.tsx
@@ -1,8 +1,13 @@
+import { Clapperboard, PenToolIcon, Pentagon, Video } from "lucide-react";
 import Image from "next/image";
 
 export default function HeroSetion() {
   return (
-    <div id="about" className="max-w-7xl min-w-[300px] mx-auto px-4 flex flex-col gap-4 md:gap-8 justify-center items-center">
+    <div id="about" className="max-w-7xl min-w-[300px] mx-auto px-4 flex flex-col gap-4 md:gap-8 justify-center items-center relative">
+      <Video className="absolute top-1/4 -mt-10 left-10 z-[-1] w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16 opacity-15 -rotate-12" />
+      <Clapperboard className="absolute mt-10 top-1/4 right-20 z-[-1] w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16 opacity-15 rotate-12" />
+      <PenToolIcon className="absolute bottom-1/4 -mb-10 right-10 z-[-1] w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16 opacity-15 rotate-12" />
+      <Pentagon className="absolute bottom-1/4 mb-10 left-32 z-[-1] w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16 opacity-15 -rotate-12" />
       <div className="personal-image mt-8 md:mt-20 rounded-md shadow-xl bounce-top">
         <Image
           src="/assets/personal-image.png"
@@ -25,7 +30,7 @@ export default function HeroSetion() {
       <a
         href="https://drive.google.com/file/d/1Barq_EwXw8inayfzut2piEvLukozJzVU/view?usp=sharing"
         target="_blank"
-        className="bg-violet-400/0 border border-white/20 hover:bg-violet-300 text-white font-bold py-4 px-8 rounded-full transition-all"
+        className="heartbeat bg-violet-400/0 border border-white/20 hover:bg-violet-300 text-white font-bold py-4 px-8 rounded-full transition-all"
       >
         view CV
       </a>
