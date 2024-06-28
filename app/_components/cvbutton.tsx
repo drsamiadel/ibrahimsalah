b1197d463cddcd16c2face0d6774ef0656e587d7
Sami Adel <sami.adel.galal02@gmail.com>
2024-06-28 16:40:43 +0300

    update CV link

--- app/_components/cvbutton.tsx
+++ app/_components/cvbutton.tsx
@@ -3,7 +3,7 @@ import { ExternalLink } from "lucide-react";
 export default function CVButton() {
   return (
     <a
-      href="https://drive.google.com/file/d/1AjUAJGu5zEhadl8RC3yRY5LHXVextU0V/view"
+      href="https://drive.google.com/file/d/1eewpcqMNSL7Eh9srbiA7VWkANMUanXzO/view"
       target="_blank"
       className="heartbeat bg-violet-400/0 border border-white/20 hover:bg-violet-300 text-white font-bold py-4 px-14 rounded-full transition-all flex items-center gap-x-2 justify-center"
     >
