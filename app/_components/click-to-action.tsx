4ea78fb6835a05e6cc8a164191fc00c4445417ff
Sami Adel <sami.adel.galal02@gmail.com>
2025-11-15 13:17:12 +0200

    update CV link in ClickToAction and CVButton components

--- app/_components/click-to-action.tsx
+++ app/_components/click-to-action.tsx
@@ -1,18 +1,18 @@
 export default function ClickToAction() {
-    return (
-        <div id="hire" className="p-4 max-w-7xl min-w-[300px] mx-auto my-8">
-            <div className="flex flex-col md:flex-row items-center justify-center gap-4">
-                <h2 className="text-3xl md:text-4xl font-bold text-violet-400">
-                    Want to know more?
-                </h2>
-                <a
-                    href="https://drive.google.com/file/d/1VTRUkC8wcl1gMlfVZDqddj53I0-UsR5k/view?usp=sharing"
-                    target="_blank"
-                    className="bg-violet-400 hover:bg-violet-300 text-white font-bold py-2 px-4 rounded-full"
-                >
-                    view CV
-                </a>
-                </div>
-        </div>
-    )
+  return (
+    <div id="hire" className="p-4 max-w-7xl min-w-[300px] mx-auto my-8">
+      <div className="flex flex-col md:flex-row items-center justify-center gap-4">
+        <h2 className="text-3xl md:text-4xl font-bold text-violet-400">
+          Want to know more?
+        </h2>
+        <a
+          href="https://drive.google.com/file/d/1ynWpyUwOrwkOsoYly7c9Fl5LAkB1f2te/view?usp=sharing"
+          target="_blank"
+          className="bg-violet-400 hover:bg-violet-300 text-white font-bold py-2 px-4 rounded-full"
+        >
+          view CV
+        </a>
+      </div>
+    </div>
+  );
 }
--- app/_components/cvbutton.tsx
+++ app/_components/cvbutton.tsx
@@ -3,7 +3,7 @@ import { ExternalLink } from "lucide-react";
 export default function CVButton() {
   return (
     <a
-      href="https://drive.google.com/file/d/1Di7NIs28ARWgNVVVRk0Xe5063--XFGhx/view?usp=drive_link"
+      href="https://drive.google.com/file/d/1ynWpyUwOrwkOsoYly7c9Fl5LAkB1f2te/view?usp=sharing"
       target="_blank"
       className="heartbeat bg-violet-400/0 border border-white/20 hover:bg-violet-300 text-white font-bold py-4 px-14 rounded-full transition-all flex items-center gap-x-2 justify-center"
     >
